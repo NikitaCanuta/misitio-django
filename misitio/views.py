a92113d6557bddaeceffe7ddaf460ee098fe250e
NikitaCanuta <nikitahuaman@gmail.com>
2017-01-10 13:59:30 -0500

    Desarrollo vistas hola mundo y horas

--- misitio/views.py
+++ misitio/views.py
@@ -1,4 +1,20 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
+import datetime
 
 def hola(request):
     return HttpResponse("Hola Mundo")
+
+
+def fecha_actual(request):
+    ahora = datetime.datetime.now()
+    html ="<html><body><h1>Fecha:</h1><h3>%s</h3></body></html>" %ahora
+    return HttpResponse(html)
+
+def horas_adelante(request, horas):
+    try:
+        horas = int(horas)
+    except ValueError:
+        raise Http404()
+    dt= datetime.datetime.now() + datetime.timedelta(hours=horas)
+    html ="<html><body><h1>En %s horas(s), seran</h1><h3>%s</h3></body></html>" %(horas, dt)
+    return HttpResponse(html)
