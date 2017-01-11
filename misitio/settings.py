deb409570a45d52cb788ed4827d17a3e1ec1daf7
NikitaCanuta <nikitahuaman@gmail.com>
2017-01-11 00:27:31 -0500

    Agrege biblioteca

--- misitio/settings.py
+++ misitio/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'biblioteca',
 ]
 
 MIDDLEWARE_CLASSES = [
@@ -76,8 +77,12 @@ WSGI_APPLICATION = 'misitio.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'biblioteca',
+        'USER': 'postgres',
+        'PASSWORD':'',
+        'HOST':'localhost',
+        'PORT':'5432',
     }
 }
 
@@ -119,3 +124,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_ROOT ='media/'
