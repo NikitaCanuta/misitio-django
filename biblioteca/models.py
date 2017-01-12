2ff43bf28aa489486db661a2234db9c88d097215
NikitaCanuta <nikitahuaman@gmail.com>
2017-01-12 18:20:09 -0500

    Agrege Modificaciones al biblioteca.odel

--- biblioteca/models.py
+++ biblioteca/models.py
@@ -8,10 +8,16 @@ class Editor(models.Model):
     pais                = models.CharField(max_length=50)
     website             = models.URLField()
 
+    def __str__(self):
+        return self.nombre
+
 class Autor(models.Model):
     nombre              = models.CharField(max_length=30)
     apellidos           = models.CharField(max_length=40)
     email               = models.EmailField()
+
+    def __str__(self):
+        return '%s %s' %(self.nombre, self.apellidos)
 
 class Libro(models.Model):
     titulo              = models.CharField(max_length=100)
@@ -19,3 +25,6 @@ class Libro(models.Model):
     editor              = models.ForeignKey(Editor)
     fecha_publicacion   = models.DateField()
     portada             = models.ImageField(upload_to='portadas')
+
+    def __str__(self):
+        return self.tituloter
