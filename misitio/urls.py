a5bdf13b5a2e7e7cdd038b00393a61101ff60a33
NikitaCanuta <nikitahuaman@gmail.com>
2017-01-12 18:41:34 -0500

    Agregado sitio admin

--- misitio/urls.py
+++ misitio/urls.py
@@ -1,8 +1,8 @@
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
 from misitio.views import hola, fecha_actual, horas_adelante
 urlpatterns = [
-    url(r'^admin/$', admin.site.urls),
+    url(r'^admin/', admin.site.urls),
     url(r'^hola/$',hola),
     url(r'^fecha/$',fecha_actual),
     url(r'^fecha/mas/(\d{1,2})/$',horas_adelante),
